34f96673389aec584fd5ab124394ec874a8273b3
Argenis Arriojas <argearriojas@gmail.com>
2020-02-24 12:46:48 -0500

    Update ws

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,4 @@
 
-docker build -t umbibio/workshop:argenis .
+# docker build -t umbibio/reinforcement-learning reinforcement-learning/Dockerfile
+# docker build -t umbibio/reinforcement-learning:tf2 reinforcement-learning_tf2/Dockerfile
+# docker build -t umbibio/reinforcement-learning:tf1 reinforcement-learning_tf1/Dockerfile
--- run_container.sh
+++ run_container.sh
@@ -1,2 +1,4 @@
 
-docker run -it --rm -p8888:8888 -v="$PWD/data:/data" umbibio/workshop:argenis
+# docker run -it --rm -p8888:8888 -v="$PWD/data:/data" umbibio/reinforcement-learning
+# docker run -it --rm -p8888:8888 -v="$PWD/data:/data" umbibio/reinforcement-learning:tf2
+# docker run -it --rm -p8888:8888 -v="$PWD/data:/data" umbibio/reinforcement-learning:tf1
